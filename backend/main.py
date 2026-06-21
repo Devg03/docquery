9fb22520b015dce3934f837214fab65abbb821c3
Dev Gandhi <gandhiDX02@gmail.com>
2026-06-21 14:21:05 -0500

    db credentials set-up and .env.example added for reference

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = FastAPI()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
 
 origins = [
     "http://127.0.0.1:5173",      # Local Vite dev server
