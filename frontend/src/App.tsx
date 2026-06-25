69c11e72cd3b172c55d4e7afeea59ea98cc06373
Dev Gandhi <gandhiDX02@gmail.com>
2026-06-24 20:13:43 -0500

    functionality to select and upload file- frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,9 @@ function App() {
   const [count, setCount] = useState(0)
   const [ status, setStatus ] = useState("loading...")
 
+  const [ file, setFile ] = useState(null)
+  const [ uploadStatus, setUploadStatus ] = useState("")
+
   useEffect(() => {
     async function loadHealth() {
       try {
@@ -20,9 +23,41 @@ function App() {
     loadHealth()
   }, [])
 
+  function handleFileChange(event) {
+    if (event.target.files && event.target.files[0]) {
+      setFile(event.target.files[0])
+    }    
+  }
+
+  async function sendToUpload(file) {
+
+    if (!file) {
+      setStatus("Please choose a file first.")
+      return
+    }
+
+    const formData = new FormData()
+    formData.append("file", file)
+    
+    try {
+      const response = await fetch('http://localhost:8000/upload', {
+        method:"POST",
+        body: formData,
+      })
+      const data = await response.json()
+      setStatus(`Uploaded ${data.filename} - ${data.num_chunks} chunks`)
+    } catch (error) {
+      setStatus("Upload failed. Is the backend running?")
+    }
+
+  }
+
+
   return (
     <div>
-      <h1>Backened status: {status}</h1>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={() => sendToUpload(file)}>Upload</button>
+      <p>{status}</p>
     </div>
   )
 }
